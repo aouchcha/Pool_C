08212b0be79aa4f7ab608e4acd26fa75775384fe
Ayamne <ouchchateaymane@gmail.cpm>
2025-11-29 00:07:24 +0100

    finishing the two sum

---
+++ Day_05/twosum.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void two_sum(int arr[], int size, int target) {
+    for (int i=0; i < size; i++) {
+        for (int j=i+1; j < size; j++) {
+            if (arr[i] + arr[j] == target) {
+                printf("[%d, %d]\n",i,j);
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[] = {2,7,11,15,0,22};
+    int target = 22;
+    int size = sizeof(arr)/sizeof(arr[0]);
+    two_sum(arr, size, target);
+}
